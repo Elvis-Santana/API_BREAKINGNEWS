89cfe1b54898eaa1519e6423815175736d6a343b
Elvis Santana <elvissantanaguimaraes@gmail.com>
2023-11-07 11:08:06 -0300

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import "dotenv/config";
-const app = express();
-const PORT = Number(process.env.PORT);
-app.get("/Home", (req, res) => {
-    res.send("Hello World");
-});
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
